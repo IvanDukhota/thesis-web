c40a03ce7761dbaf2e8775ffda553a180c86d711
Ivan Dukhota <ivan.dukhota@gmail.com>
2026-04-18 16:02:16 +0300

    Create and modify main page + create header logic

--- frontend/src/pages/MainPage/MainPage.jsx
+++ frontend/src/pages/MainPage/MainPage.jsx
@@ -1,9 +1,38 @@
 import './MainPage.css';
+import { useEffect } from "react";
+import Header from '../../components/Header/Header';
+
+import DarkVeil from '../../components/DarkVeil/DarkVeil';
+import AnimatedContent from '../../components/AnimatedContent/AnimatedContent';
+import ShinyText from '../../components/ShinyText/ShinyText';
+import { darkVeilConfig, shinyTextConfig, firstAnimation, secondAnimation, thirdAnimation } from './MainPageConfig.js';
 
 function MainPage() {
+
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, []);
+
     return (
         <div className="mainpage">
-            <h1 className='test-text'>Hello world!</h1>
+            <div className="mainpage-bg">
+                <DarkVeil {...darkVeilConfig} />
+            </div>
+            <Header />
+            <div className='welcome-container'>
+                <AnimatedContent {...firstAnimation}>
+                    <h1 className='text-type1'>TeamHub Workspace For Best Developers</h1>
+                </AnimatedContent>
+                <AnimatedContent {...secondAnimation}>
+                    <ShinyText {...shinyTextConfig}></ShinyText>
+                </AnimatedContent>
+                <AnimatedContent {...thirdAnimation}>
+                    <div className='button-block'>
+                        <button className='button-class1'>Learn More 🛈</button>
+                        <button className='button-class2'>Get Started ↗</button>
+                    </div>
+                </AnimatedContent>
+            </div>
         </div>
     );
 }
