1381826af4bd6a11fb34402dd437cf4bba57decc
Ivan Dukhota <ivan.dukhota@gmail.com>
2026-04-18 14:57:20 +0300

    Fix frontend build error

---
+++ frontend/src/pages/MainPage/MainPage.jsx
@@ -0,0 +1,11 @@
+import './MainPage.css';
+
+function MainPage() {
+    return (
+        <div className="mainpage">
+            <h1 className='test-text'>Hello world!</h1>
+        </div>
+    );
+}
+
+export default MainPage;
